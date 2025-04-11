5a91c30ca54d2dcf66b954cb0ecb502250f6c05d
TranPhatDat <dattp3010@gmail.com>
2025-04-11 16:14:03 +0700

    update v8

--- routes/vouchers.js
+++ routes/vouchers.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const { addVoucher, deleteVoucher, updateVoucher, getAllVouchers } = require('../controllers/voucher');
+let {check_authentication,check_authorization} = require('../utils/check_auth')
+
 
 const router = express.Router();
 
 // Route để lấy danh sách tất cả các voucher
-router.get('/', getAllVouchers); // Lấy tất cả voucher
+router.get('/',check_authentication,check_authorization(["admin","mod"]), getAllVouchers); // Lấy tất cả voucher
 
 // Route để thêm một voucher mới
-router.post('/', addVoucher); // Thêm voucher
+router.post('/',check_authentication,check_authorization(["admin","mod"]), addVoucher); // Thêm voucher
 
 // Route để xóa một voucher theo ID
-router.delete('/:id', deleteVoucher); // Xóa voucher
+router.delete('/:id',check_authentication,check_authorization(["admin","mod"]), deleteVoucher); // Xóa voucher
 
 // Route để cập nhật một voucher theo ID
-router.put('/:id', updateVoucher); // Sửa voucher
+router.put('/:id',check_authentication,check_authorization(["admin","mod"]), updateVoucher); // Sửa voucher
 
 module.exports = router;
